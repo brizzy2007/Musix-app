e568e6531e10b1271fad6af86f0ff858cc8a2d75
brizzy2007 <brightdavid951@gmail.com>
2024-02-04 13:24:31 +0100

    adding aos

--- src/Homeinnerpages/Innerpage_4.jsx
+++ src/Homeinnerpages/Innerpage_4.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Main from '../assets/Main.jpg';
 import Main2 from '../assets/Main2.jpg';
 import Main3 from '../assets/Main3.jpg';
 import Main4 from '../assets/Main4.jpg';
 import './Innerpage_4.css';
 import { IoLogoYoutube } from "react-icons/io";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Innerpage_4 = ({ text, texts }) =>{
+  useEffect(() => {
+    console.log('AOS initialization started');
+    AOS.init({
+      duration: 1000, // You can adjust the duration as needed
+      once: true, // Animation will only happen once
+    });
+  }, []);
+
+
   return (
     <div className='lg:flex gap-2 mt-16 sm:block'>
-    <div className="image-container">
+    <div className="image-container" data-aos="fade-up">
       <img src={Main} alt="Hoverable Image" className="hoverable-imag"/>
       <div className="overlay">
         <p> <IoLogoYoutube /> {text}</p>
       </div>
     </div>
 
-    <div className="image-container">
+    <div className="image-container" data-aos="fade-up">
       <img src={Main2} alt="Hoverable Image" className="hoverable-image" />
       <div className="overlay">
         <p> <IoLogoYoutube /> {texts}</p>
       </div>
     </div>
 
-    <div className="image-container">
+    <div className="image-container" data-aos="fade-up">
       <img src={Main3} alt="Hoverable Image" className="hoverable-image" />
       <div className="overlay">
         <p> <IoLogoYoutube /> {text}</p>
       </div>
     </div>
 
-    <div className="image-container">
+    <div className="image-container" data-aos="fade-up">
       <img src={Main4} alt="Hoverable Image" className="hoverable-image" />
       <div className="overlay">
         <p> <IoLogoYoutube /> {text}</p>
